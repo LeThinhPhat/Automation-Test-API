ca37851f5464158b5770e6b5ec2e390e74ca21a6
LeThinhPhat <lethinhphat7102003@gmail.com>
2026-05-03 23:52:30 +0700

    End to End mix API and UI

--- tests/WebAPITest.spec.js
+++ tests/WebAPITest.spec.js
@@ -3,6 +3,12 @@ const loginPayload = {
   userEmail: "lethinhphat7102003@gmail.com",
   userPassword: "Phat123456",
 };
+const orderPayload = {
+  orders: [
+    { country: "VietNam", productOrderedId: "62023a7616fcf72fe9dfc619" },
+  ],
+};
+let token;
 
 test.beforeEach(async () => {
   const apiContext = await request.newContext();
@@ -15,5 +21,18 @@ test.beforeEach(async () => {
   expect(loginResponse.ok()).toBeTruthy();
   const loginResponseBody = await loginResponse.json();
   const token = loginResponseBody.token;
+  console.log("Token:", token);
+
+  apiContext.post(
+    "https://www.rahulshettyacademy.com/api/ecom/orders/create-order",
+
+    {
+      data: orderPayload,
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    },
+  );
 });
 test.beforeAll(async () => {});
